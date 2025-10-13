d2917f7f8c9b482a4283d882f9b257cf8a59c879
abhinavprakash-x <abhinavprakash.13x@gmail.com>
2025-10-13 21:57:52 +0530

    Day 3

---
+++ 13-10-2025/day3q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter First Number: ";
+    cin>> a;
+    cout<<"Enter Second Number: ";
+    cin>> b;
+    int product = a*b;
+    cout<<"Product: "<< product;
+    return 0;
+};
---
+++ 13-10-2025/day3q2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter First Number: ";
+    cin>> a;
+    cout<<"Enter Second Number: ";
+    cin>> b;
+    int diff = a-b;
+    cout<<"Difference: "<< diff;
+    return 0;
+};
