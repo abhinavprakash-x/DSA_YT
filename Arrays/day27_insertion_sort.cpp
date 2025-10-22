c1bc14a654080fa155de0b0bd9c2af970863a9e8
abhinavprakash-x <abhinavprakash.13x@gmail.com>
2025-10-22 08:50:34 +0530

    Day 27

--- Arrays/day27_insertion_sort.cpp
+++ Arrays/day27_insertion_sort.cpp
@@ -2,15 +2,37 @@
 using namespace std;
 
 void insertion_sort(int arr[], int n){
-    //
+    for(int i = 1; i < n; ++i){
+        for(int j = i; j > 0; --j){
+            if(arr[j] < arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            } else {
+                break;
+            }
+        }
+    }
 }
 
 void descending_insertion_sort(int arr[], int n){
-    //
+    for(int i = 1; i < n; ++i){
+        for(int j = i; j > 0; --j){
+            if(arr[j] > arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            } else {
+                break;
+            }
+        }
+    }
 }
 
-void reverse_insertion_sort(int arr[], int n){
-    //
+void reverse_insertion_sort(int arr[], int n) {
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = i; j < n - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            } else break;
+        }
+    }
 }
 
 void print_array(int arr[], int n){
